4e8d39bbf3c425f174e1f42ce78378a6dc2c723d
pptang <pptang@mercari.com>
2021-02-16 22:13:51 +0900

    Fill text for request parameters

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -18,12 +19,22 @@ app.post('/generate-ikigai', async (req, res) => {
   const img = await loadImage('./ikigai.png');
   ctx.drawImage(img, 15, 30);
   ctx.font = '16px serif';
-  ctx.fillText('Hello world', 368, 168);
+  const data = req.body;
+  ctx.fillText(data.love, 397, 207);
+  ctx.fillText(data.happy, 402, 429);
+  ctx.fillText(data.skill, 621, 418);
+  ctx.fillText(data.help, 479, 330);
+  ctx.fillText(data.inspire, 302, 323);
+  ctx.fillText(data.angry, 470, 503);
+  ctx.fillText(data.sell, 380, 596);
+  ctx.fillText(data.job, 308, 495);
   const buffer = canvas.toBuffer('image/png');
   fs.writeFileSync('./public/image.png', buffer);
-  res.json({
-    imgUrl: 'http://localhost:3000/image.png',
-  });
+  res.json(
+    JSON.stringify({
+      imgUrl: 'http://localhost:3000/image.png',
+    })
+  );
 });
 
 app.listen(port, () => {
