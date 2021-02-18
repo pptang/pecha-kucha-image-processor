4892d9bc1cf4446d57169ecfde18bbf2e51f9646
pptang <pptang@mercari.com>
2021-02-18 21:14:24 +0900

    fix: key name

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
 
