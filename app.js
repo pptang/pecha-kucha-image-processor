dd7c52cd2ce8762db7ed13d2cd439a3608e8aa97
pptang <pptang@mercari.com>
2021-03-28 14:35:31 +0900

    test: req body

--- app.js
+++ app.js
@@ -220,7 +220,7 @@ app.post('/generate-preference-chart', async (req, res) => {
     energy,
     exercise,
   } = req.body;
-
+  console.log(req.body);
   // 2. Retrieve current preference in DB
 
   const preferenceRef = db.collection('preference').doc(channelId);
